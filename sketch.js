8744b3f2e3d25784184413c9856a2b1e5c6227e4
Amogh-Sharma1 <86812220+Amogh-Sharma1@users.noreply.github.com>
2021-07-21 18:58:45 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var S1,S2
+function setup() {
+  createCanvas(400,400);
+S1 = new student("Amogh",11,5.2)
+S2 = new student("Akanksha",11,5.3)
+S1.fly()
+S2.fly()
+
+}
+
+function draw() 
+{
+  background(30);
+}
---
+++ student.js
@@ -0,0 +1,12 @@
+class student{
+    constructor(name,age,height){
+        this.name = name
+        this.age = age
+        this.height = height
+    }
+    fly(){
+    console.log(this.name)
+    console.log(this.age)
+    console.log(this.height)
+}
+}
